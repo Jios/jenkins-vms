37b301222b563f8e566fb1d3a1c946cba1ad3465
jian li <jian_li@tutk.com>
2016-08-23 21:50:22 +0800

    template: update swarm command with master ip (host variable)

--- provisioning/geerlingguy.jenkins/templates/groovy/run-swarm.groovy
+++ provisioning/geerlingguy.jenkins/templates/groovy/run-swarm.groovy
@@ -1,4 +1,4 @@
-def command = "java -jar /var/lib/jenkins/swarm/swarm-client-2.2-jar-with-dependencies.jar &"
+def command = "java -jar /var/lib/jenkins/swarm/swarm-client-2.2.jar -master http://{{ hostvars.master.ansible_ssh_host }}:8080/ -username admin -password admin -name Ubuntu -labels linux &"
 def process = command.execute()
 //process.waitFor()
 
