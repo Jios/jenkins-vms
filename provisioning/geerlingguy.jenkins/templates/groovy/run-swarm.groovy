54a44875d64229d91249eed1742a312ad39da2f4
jian <li822jh@gmail.com>
2016-09-05 23:22:58 +0800

    template: remove swarm client

--- provisioning/geerlingguy.jenkins/templates/groovy/run-swarm.groovy
+++
@@ -1,7 +0,0 @@
-def command = "java -jar /var/lib/jenkins/swarm/swarm-client-2.2.jar -master http://{{ hostvars.master.ansible_ssh_host }}:8080/ -username {{ jenkins_admin_username }} -password {{ jenkins_admin_password }} -name Coolie -labels linux &"
-
-def process = command.execute()
-//process.waitFor()
-
-//def output = process.in.text
-//log.info output
