4669ba5ee685b8fed4cb046f692be243b159060c
jian <li822jh@gmail.com>
2016-09-07 23:25:02 +0800

    template: add android SDK environment variables to script

--- provisioning/FGtatsuro.android/templates/swarm_client.sh
+++ provisioning/FGtatsuro.android/templates/swarm_client.sh
@@ -9,6 +9,11 @@
 # Description: Auto run swarm-client.jar
 ### END INIT INFO
 
+export ANDROID_HOME="{{ android_home }}"
+export ANDROID_TOOLS=$ANDROID_HOME/tools/
+export ANDROID_PLATFORM_TOOLS=$ANDROID_HOME/platform-tools/
+export PATH=$PATH:$ANDROID_TOOLS:$ANDROID_PLATFORM_TOOLS
+
 java -jar "{{ swarm_client_path }}/{{ swarm_client }}" \
 	 -master "http://{{ hostvars.master.ansible_ssh_host }}:8080/" \
 	 -username {{ jenkins_admin_username }} -password {{ jenkins_admin_password }} \
